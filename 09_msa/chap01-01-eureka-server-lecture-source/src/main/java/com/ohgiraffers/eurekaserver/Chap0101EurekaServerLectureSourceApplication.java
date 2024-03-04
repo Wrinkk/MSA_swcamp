9d9b53547b9eb21c1e2d428efbe4be016263c8f6
LAPTOP-SB0CM2VU\Playdata <wjdxodndjaak@naver.com>
2024-03-04 12:06:58 +0900

    MSA (first, second, gateway 설정) gateway를 항상 마지막으로 켜야하며 ,filter를 사용하여 헤더를 보낼수있음.

--- 09_msa/chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
+++ 09_msa/chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+
+/* 설명. 유레카 서버의 자격을 등록(서버스 디스커버리로써 지금 프로젝트 기동) */
 @EnableEurekaServer
 public class Chap0101EurekaServerLectureSourceApplication {
 
---
+++ 09_msa/chap02-02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/SecondServiceController.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+//@RequestMapping("/second-service")
+@RequestMapping("/")
+public class SecondServiceController {
+
+    @GetMapping("health_check")
+    public String healthCheck() {
+        return "OK2";
+    }
+
+    @GetMapping("message")
+    public String message(@RequestHeader("second-request") String header) {
+        return "Second Service Message";
+    }
+
+
+
+}
